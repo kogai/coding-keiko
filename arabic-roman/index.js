192eb6b7d416758228d69f061982b9da227c62bf
Shinichi Kogai <kogai0121@gmail.com>
2018-12-11 12:11:18 +0900

    solved

--- arabic-roman/index.js
+++ arabic-roman/index.js
@@ -18,50 +18,53 @@ const romanToArabic = {
 };
 
 const convert = ([...romans]) => {
-    try {
-      const result = romans
-        .map((r, i) => {
-          const next = romanToArabic[romans[i + 1]] || 0;
-          const current = romanToArabic[r];
-          return current;
-        })
-        .reduce((acc, x) => {
-          const lastIndex = acc.length - 1;
-          const lastChunk = acc[lastIndex];
-          if (!!lastChunk && x === lastChunk[0]) {
-            lastChunk.push(x);
-            acc[lastIndex] = lastChunk;
-            return acc;
-          }
-          acc.push([x]);
+  try {
+    const result = romans
+      .map((r, i) => {
+        const next = romanToArabic[romans[i + 1]] || 0;
+        const current = romanToArabic[r];
+        return current;
+      })
+      .reduce((acc, x) => {
+        const lastIndex = acc.length - 1;
+        const lastChunk = acc[lastIndex];
+        if (!!lastChunk && x === lastChunk[0]) {
+          lastChunk.push(x);
+          acc[lastIndex] = lastChunk;
           return acc;
-        }, [])
-        .map((x, i, xs) => {
-          const next = xs[i + 1] || 0;
-          const current = x;
-          if (next > current) {
-            if (current === 1 && ![5, 10].includes(next)) {
-              throw new Error("Invalid");
-            }
-            if (current === 10 && ![50, 100].includes(next)) {
-              throw new Error("Invalid");
-            }
-            if (current === 100 && ![500, 1000].includes(next)) {
-              throw new Error("Invalid");
-            }
-            return current;
-          } else if (next === current && [5, 50, 500].includes(current)) {
+        }
+        acc.push([x]);
+        return acc;
+      }, [])
+      .map((x, i, xs) => {
+        const next = (xs[i + 1] || [0])[0];
+        const current = x[0];
+        if (next > current) {
+          if (current === 1 && ![5, 10].includes(next)) {
             throw new Error("Invalid");
           }
-        })
-        .reduce((acc, x) => acc + x, 0);
-          return result;
+          if (current === 10 && ![50, 100].includes(next)) {
+            throw new Error("Invalid");
+          }
+          if (current === 100 && ![500, 1000].includes(next)) {
+            throw new Error("Invalid");
+          }
+          if (x.length > 1) {
+            throw new Error("Invalid");
+          }
+          return -current;
+        } else if ([5, 50, 500].includes(current) && x.length > 1) {
+          throw new Error("Invalid");
         }
-      catch (e) {
-        return e.message;
-      }
-    };
+        return current * x.length;
+      })
+      .reduce((acc, x) => acc + x, 0);
+    return result;
+  } catch (e) {
+    return e.message;
+  }
+};
 
-    module.exports = {
-      convert
-    };
+module.exports = {
+  convert
+};
