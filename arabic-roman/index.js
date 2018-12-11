1ef9b15ac7e5fbff07522a57d6b69a01416fd131
Masaya Nakamura <m-nakamura@feedforce.jp>
2018-12-11 11:34:23 +0900

    wip

--- arabic-roman/index.js
+++ arabic-roman/index.js
@@ -18,17 +18,32 @@ const romanToArabic = {
 };
 
 const convert = ([...romans]) => {
-  const result = romans
-    .map((r, i) => {
-      const next = romanToArabic[romans[i + 1]] || 0;
-      const current = romanToArabic[r];
-      if (next > current) {
-        return -current;
-      }
-      return current;
-    })
-    .reduce((acc, x) => acc + x, 0);
-  return result;
+  try {
+    const result = romans
+      .map((r, i) => {
+        const next = romanToArabic[romans[i + 1]] || 0;
+        const current = romanToArabic[r];
+        if (next > current) {
+          if (current === 1 && ![5, 10].includes(next)) {
+            throw new Error('Invalid');
+          }
+          if (current === 10 && ![50, 100].includes(next)) {
+            throw new Error('Invalid');
+          }
+          if (current === 100 && ![500, 1000].includes(next)) {
+            throw new Error('Invalid');
+          }
+          return -current;
+        } else if (next === current && [5,  50, 500].includes(current)) {
+          throw new Error('Invalid');
+        }
+        return current;
+      })
+      .reduce((acc, x) => acc + x, 0);
+    return result;
+  } catch (e) {
+    return e.message;
+  }
 };
 
 module.exports = { convert };
