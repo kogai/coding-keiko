f017ee79af5dcf7ed185a0d74417917c43b0d6bb
Itaru Kosaka <kosaka@feedforce.jp>
2018-12-12 11:28:40 +0900

    implement 1

--- ocr/index.js
+++ ocr/index.js
@@ -5,6 +5,14 @@
  */
 const ocr = str => {
   const lines = str.split("\n").filter(s => s !== "");
+  
+  lines.reduce((acc, line, i) => {
+    console.log(line);
+    if (i % 3 == 0) {
+      return acc;
+    }
+    return acc;
+  }, []);
   console.log(lines);
 };
 
