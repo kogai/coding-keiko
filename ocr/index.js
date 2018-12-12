a34c252eb80455838b396b23126a242e6cbbd4a6
Shinichi Kogai <kogai0121@gmail.com>
2018-12-12 11:39:39 +0900

    wip

--- ocr/index.js
+++ ocr/index.js
@@ -5,15 +5,27 @@
  */
 const ocr = str => {
   const lines = str.split("\n").filter(s => s !== "");
-  
-  lines.reduce((acc, line, i) => {
-    console.log(line);
+  //  _  _  _  _  _  _  _  _  _
+  // | || || || || || || || || |
+  // |_||_||_||_||_||_||_||_||_|
+
+  const result = lines.reduce((acc, [...line], i) => {
+    for (let i = 0; i < 9; i++) {
+      // const element = array[index];
+      const el1 = line.splice(i, 3);
+      // const el2 = line.pop();
+      // const el3 = line.pop();
+      acc[i] ? acc[i].push(el1) : (acc[i] = [el1]);
+      // console.log(el1, el2, el3);
+    }
+    // console.log(line.length);
+    // console.log(line[0], line[1], line);
     if (i % 3 == 0) {
       return acc;
     }
     return acc;
   }, []);
-  console.log(lines);
+  console.log(result);
 };
 
 module.exports = { ocr };
