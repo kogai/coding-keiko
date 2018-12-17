19cc1bc6b0666c889bc93def8c436e0107a43bc6
Shinichi Kogai <kogai0121@gmail.com>
2018-12-17 15:00:25 +0900

    :pencil: Add retrospective

--- bowling-score/index.js
+++ bowling-score/index.js
@@ -11,6 +11,7 @@ const scores = n => {
   }
 };
 
+<<<<<<< Updated upstream
 const normalize = ([x, ...params]) => {
   if (params.length === 0) {
     return [];
@@ -39,6 +40,32 @@ const normalize = ([x, ...params]) => {
 //     },
 //     { index: 0, frames: [[], [],[],[],[],[],[],[],[], []] }
 //   ).frames;
+=======
+const normalize = ([...params]) =>
+  params.reduce(
+    (acc, x, i) => {
+      if (x === "X") {
+        acc.index = acc.index + 1;
+        if (acc.index === 10) {
+          acc.frames.push([10, scores(params[i + 1]), scores(params[i + 2])]);
+        } else if (acc.index > 10) {
+        } else {
+          acc.frames.push([10, 0]);
+        }
+      } else {
+        // if (acc.frames[acc.index].length < 2) {
+        console.log(acc.frames[acc.index - 1]);
+        acc.index = acc.index + 1;
+        acc.frames.push([scores(x), scores(params[i + 1])]);
+        // } else {
+        // }
+      }
+      // console.log(scores[x]);
+      return acc;
+    },
+    { index: 0, frames: [] }
+  ).frames;
+>>>>>>> Stashed changes
 
 const calculate = ([...params]) => {
   return params.map(p => {
